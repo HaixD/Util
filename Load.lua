f779b291e0e8f54618b806df078d6c4f52db17ea
HaixD <103500732+HaixD@users.noreply.github.com>
2023-06-11 23:00:23 +0700

    Add files via upload

---
+++ Load.lua
@@ -0,0 +1,5 @@
+local utils = {
+    Webhook = loadstring(game:HttpGet("https://raw.githubusercontent.com/HaixD/Util/main/Webhook.lua"))(),
+    CTween = loadstring(game:HttpGet("https://raw.githubusercontent.com/HaixD/Util/main/CTween.lua"))(),
+}
+return utils
---
+++ Webhook.lua
@@ -0,0 +1,61 @@
+local Request = (syn and syn.request or request or http and http.request or http_request) or error("No request function")
+local HttpService = game:GetService("HttpService")
+local Lib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/HaixD/notify-lib/main/lib'),true))()
+
+local Webhook = {}
+
+function Webhook:Send(WebhookUrl, Message)
+    local Success, Error = pcall(function()
+        local Data = {
+            ["content"] = Message
+        }
+        Request({
+            Url = WebhookUrl,
+            Method = "POST",
+            Headers = {
+                ["Content-Type"] = "application/json"
+            },
+            Body = HttpService:JSONEncode(Data)
+        })
+    end)
+
+    if not Success then
+        Lib.prompt("Error", "" .. Error, 5)
+    else
+        --working
+    end
+end
+
+function Webhook:Embed(WebhookUrl, Content, Title, description)
+    local Success, Error = pcall(function()
+        local Data = Request(
+            {
+                Url = WebhookUrl,
+                Method = "POST",
+                Headers = {
+                    ["Content-Type"] = "application/json"
+                },
+                Body = HttpService:JSONEncode(
+                    {
+                        ["content"] = Content,
+                        ["embeds"] = {
+                            {
+                                ["title"] = Title,
+                                ["description"] = description
+                            }
+                        }
+                    }
+                )
+            }
+        )
+    end)
+
+    if not Success then
+        Lib.prompt("Error", "" .. Error, 5)
+    else
+        --working
+    end
+
+end
+
+return Webhook
